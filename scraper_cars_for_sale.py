3043e4afcc76d2522794b36cd5409759bbe18a88
Peter D'Elia <petertdelia@gmail.com>
2019-06-26 11:44:41 -0400

    minor changes

--- scraper_cars_for_sale.py
+++ scraper_cars_for_sale.py
@@ -13,7 +13,7 @@ def get_site(url):
     return driver
 
 def make_soup(driver):
-    soup = BeautifulSoup(driver.page_source)
+    soup = BeautifulSoup(driver.page_source,features="html.parser")
     return soup
 
 def init_car_list():
@@ -23,15 +23,12 @@ def init_car_list():
 def populate_temp_car_list(soup):
     name_containers = soup.findAll("a", {"class":"vehicle-name"})
     temp_car_list = [[] for i in range(len(name_containers))]
-    print('length of name_containers is' + str(len(name_containers)))
-    print('length of temp_car_list is' + str(len(temp_car_list)))
     i=0
     for index in name_containers:  
         name = name_containers[i].h4.text  
         temp_car_list[i] = name.split()
         i+=1
     mileages = soup.findAll("div",{"class":"specs-miles"})
-    print('length of mileages is' + str(len(mileages)))
     for i in range(len(mileages)): 
         mileages[i] = mileages[i].text.split()
         mileage = mileages[i]
@@ -44,6 +41,7 @@ def populate_temp_car_list(soup):
 
 def append_to_car_list(temp_car_list,car_list):
     car_list.extend(temp_car_list)
+    print(len(car_list))
     return car_list
 
 def go_to_next_page(driver):
@@ -53,39 +51,15 @@ def go_to_next_page(driver):
     except:
         print('an error occurred when clicking to the next page')
 
-def make_car_list(num_pages):
-    car_list = []
-    for i in range(1,num_pages + 1): 
-        url = 'https://www.carsforsale.com/Search?Make=Ford&Model=Edge&Conditions=used&PageNumber=1&OrderBy=Relevance&OrderDirection=Desc'
-        soup_html = make_soup_selenium(url)
-        name_containers = soup_html.findAll("a", {"class":"vehicle-name"})
-        temp_car_list = [[] for i in range(len(name_containers))]
-        print('length of name_containers is' + str(len(name_containers)))
-        print('length of temp_car_list is' + str(len(temp_car_list)))
-        i=0
-        for index in name_containers:  
-            name = name_containers[i].h4.text  
-            temp_car_list[i] = name.split()
-            i+=1
-        mileages = soup_html.findAll("div",{"class":"specs-miles"})
-        print('length of mileages is' + str(len(mileages)))
-        for i in range(len(mileages)): 
-            mileages[i] = mileages[i].text.split()
-            mileage = mileages[i]
-            mileages[i] = mileage[0]
-            try:
-                temp_car_list[i].append(mileages[i])
-            except IndexError:
-                print('index out of range--oops!')
-            
-        car_list.extend(temp_car_list)
-    return car_list
-
 url = 'https://www.carsforsale.com/Search?Make=Ford&Model=Edge&Conditions=used&PageNumber=1&OrderBy=Relevance&OrderDirection=Desc'
 
-driver = get_site(url)
-soup = make_soup(driver)
-car_list = init_car_list()
-temp_car_list = populate_temp_car_list(soup)
-car_list = append_to_car_list(temp_car_list,car_list)
-len(car_list)
+def put_it_together():
+    driver = get_site(url)
+    soup = make_soup(driver)
+    car_list = init_car_list()
+    temp_car_list = populate_temp_car_list(soup)
+    car_list = append_to_car_list(temp_car_list,car_list)
+    go_to_next_page(driver)
+    driver.close()
+
+put_it_together()
