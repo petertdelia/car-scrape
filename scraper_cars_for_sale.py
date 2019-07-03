19b38f38028bede6443fbc2fa412e53f5eb98fdb
Peter D'Elia <petertdelia@gmail.com>
2019-07-03 11:32:04 -0400

    added functionality to cars for sale

--- scraper_cars_for_sale.py
+++ scraper_cars_for_sale.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from time import sleep
 
 from bs4 import BeautifulSoup
 
@@ -37,6 +38,7 @@ def populate_temp_car_list(soup):
             temp_car_list[i].append(mileages[i])
         except IndexError:
             print('index out of range--oops!')
+    print(temp_car_list[0])
     return temp_car_list
 
 def append_to_car_list(temp_car_list,car_list):
@@ -48,18 +50,26 @@ def go_to_next_page(driver):
     try:
         element = driver.find_element_by_css_selector('button.btn-pagination-next')
         element.click()
+        return True
     except:
         print('an error occurred when clicking to the next page')
+        return False
 
 url = 'https://www.carsforsale.com/Search?Make=Ford&Model=Edge&Conditions=used&PageNumber=1&OrderBy=Relevance&OrderDirection=Desc'
 
-def put_it_together():
-    driver = get_site(url)
-    soup = make_soup(driver)
-    car_list = init_car_list()
-    temp_car_list = populate_temp_car_list(soup)
-    car_list = append_to_car_list(temp_car_list,car_list)
-    go_to_next_page(driver)
-    driver.close()
+url = 'https://www.carsforsale.com/Search?SearchTypeID=2&Make=Ford&Model=Edge&BodyStyle=&SubBodyStyle=&MinModelYear=2015&MaxModelYear=2015&MinPrice=&MaxPrice=&FromEstimatedMonthlyPayment=&ToEstimatedMonthlyPayment=&MaxMileage=&FromFuelEconomy=&Radius=&ZipCode=&State=&City=&FullStateName=&Latitude=&Longitude=&Conditions=Used&Conditions=Manufacturer+Certified&Conditions=Repairable&HideRepairable=&FilterImageless=&PricedVehiclesOnly=&OrderBy=Relevance&OrderDirection=desc&PageResultSize=15&PageNumber=1&TotalRecords=&FromDate=&ToDate=&DaysListed=&SourceId=&SourceExternalUserID='
 
-put_it_together()
+def make_complete_list(url):
+    driver = get_site(url)
+    car_list = init_car_list()
+    flag = True
+    while (flag == True):
+        sleep(1)
+        soup = make_soup(driver)
+        temp_car_list = populate_temp_car_list(soup)
+        car_list = append_to_car_list(temp_car_list,car_list)
+        flag = go_to_next_page(driver)
+    driver.close()
+    return car_list
+
+
