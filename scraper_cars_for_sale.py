56adab312438c38d8111af7368246fae7687d464
Peter D'Elia <petertdelia@gmail.com>
2019-07-12 09:35:58 -0400

    modified scraper

--- scraper_cars_for_sale.py
+++ scraper_cars_for_sale.py
@@ -87,7 +87,7 @@ def go_to_next_page(driver):
 url = 'https://www.carsforsale.com/Search?Make=Ford&Model=Edge&Conditions=used&PageNumber=1&OrderBy=Relevance&OrderDirection=Desc'
 
 # ford edges near 22903 
-url = 'https://www.carsforsale.com/Search?SearchTypeID=2&Make=Ford&Model=Edge&BodyStyle=&SubBodyStyle=&MinModelYear=&MaxModelYear=&MinPrice=&MaxPrice=&FromEstimatedMonthlyPayment=&ToEstimatedMonthlyPayment=&MaxMileage=&FromFuelEconomy=&Radius=100&ZipCode=22903&State=&City=&FullStateName=&Latitude=&Longitude=&Conditions=Used&Conditions=Manufacturer+Certified&Conditions=Repairable&HideRepairable=&FilterImageless=&PricedVehiclesOnly=&OrderBy=Relevance&OrderDirection=desc&PageResultSize=15&PageNumber=1&TotalRecords=&FromDate=&ToDate=&DaysListed=&SourceId=&SourceExternalUserID='
+# url = 'https://www.carsforsale.com/Search?SearchTypeID=2&Make=Ford&Model=Edge&BodyStyle=&SubBodyStyle=&MinModelYear=&MaxModelYear=&MinPrice=&MaxPrice=&FromEstimatedMonthlyPayment=&ToEstimatedMonthlyPayment=&MaxMileage=&FromFuelEconomy=&Radius=100&ZipCode=22903&State=&City=&FullStateName=&Latitude=&Longitude=&Conditions=Used&Conditions=Manufacturer+Certified&Conditions=Repairable&HideRepairable=&FilterImageless=&PricedVehiclesOnly=&OrderBy=Relevance&OrderDirection=desc&PageResultSize=15&PageNumber=1&TotalRecords=&FromDate=&ToDate=&DaysListed=&SourceId=&SourceExternalUserID='
 # 2015 ford edges
 # url = 'https://www.carsforsale.com/Search?SearchTypeID=2&Make=Ford&Model=Edge&BodyStyle=&SubBodyStyle=&MinModelYear=2015&MaxModelYear=2015&MinPrice=&MaxPrice=&FromEstimatedMonthlyPayment=&ToEstimatedMonthlyPayment=&MaxMileage=&FromFuelEconomy=&Radius=&ZipCode=&State=&City=&FullStateName=&Latitude=&Longitude=&Conditions=Used&Conditions=Manufacturer+Certified&Conditions=Repairable&HideRepairable=&FilterImageless=&PricedVehiclesOnly=&OrderBy=Relevance&OrderDirection=desc&PageResultSize=15&PageNumber=1&TotalRecords=&FromDate=&ToDate=&DaysListed=&SourceId=&SourceExternalUserID='
 
@@ -97,6 +97,7 @@ url = 'https://www.carsforsale.com/Search?SearchTypeID=2&Make=Ford&Model=Edge&Bo
 def make_complete_list(url):
     driver = get_site(url)
     car_list = init_car_list()
+    input("Press Enter to continue...")
     flag = True
     while (flag == True):
         sleep(1)
@@ -110,12 +111,8 @@ def make_complete_list(url):
 
 car_list = make_complete_list(url)
 for car in car_list:
-    if (len(car) > 7):
-        print('this car is this long (over 7 is bad): ' + str(len(car)))
-        print('we are removing this car: ' + str(car))
-        print('before we remove it, the list is this long: ' + str(len(car)))
+    if len(car) > 7:
         car_list.remove(car)
-        print('after we remove it, the list is this long: ' + str(len(car)))
 
 # make a pandas dataframe using the car list
 from pandas import DataFrame
